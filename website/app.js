1e8a9b696c48448d74988dde71769ace030a9fc9
hoytbatwit <54991313+hoytbatwit@users.noreply.github.com>
2023-04-03 14:34:44 -0400

    pushing documentation to git

--- website/app.js
+++ website/app.js
@@ -1,23 +1,27 @@
+//Code written by our group and Professor Othman
+
+//Initializing all the packages we use 
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
-
 const db = new sqlite3.Database("myDatabase.sqlite");
-
 const ping = require("ping");
-
 const { URL } = require("url");
-
 const app = express();
 app.use(express.static(__dirname));
 app.use(express.json());
 
+
+//If there is no database present one is created
 db.serialize(() => {
   db.run("CREATE TABLE IF NOT EXISTS Main (id INTEGER PRIMARY KEY, website TEXT, status TEXT, time TEXT)");
 });
 
+//function to check the status of a site
 app.get("/check-status", (req, res) => {
+  //get the website that was attached to the request
   let website = req.query.website;
 
+  //declare the website as a URL
   try {
     const url = new URL(website);
     website = url.hostname;
@@ -25,29 +29,38 @@ app.get("/check-status", (req, res) => {
     return res.status(400).json({ error: "Invalid URL" });
   }
 
+  //first we check to make sure that the URL is valid
   require("dns").resolve(website, (err) => {
     let status = "down";
 
+    //configuration for the ping
     var config = {
       timeout: 4
     };
-
+    //if there is no error meaning the url is valid we ping the site
     if (!err) {
+      //call the ping
        ping.sys.probe(website, function(isAlive){
+        //if the boolean isAlive is true then the site is up and we can tell the user that
         if(isAlive){
           status = "alive";
+          //get current time to store in database
           let temp = new Date();
           let time = temp.toLocaleTimeString();
+          //push data to database
           db.run("INSERT INTO Main (website, status, time) VALUES (?, ?, ?)", [website, status, time], (err) => {
             if (err) {
               return res.status(500).json({ error: err.message });
             }
             res.json({ website, status, time });
           });
+          //if the boolean isAlive is false then the site is down and we tell the user that
         }else{
           status = "down";
+          //get current time to store in database
           let temp = new Date();
           let time = temp.toLocaleTimeString();
+          //push data to database
           db.run("INSERT INTO Main (website, status, time) VALUES (?, ?, ?)", [website, status, time], (err) => {
             if (err) {
               return res.status(500).json({ error: err.message });
@@ -60,7 +73,7 @@ app.get("/check-status", (req, res) => {
   });
 });
 
-
+//Gets the status of the most recent database entry to display to the user since that was the status of their last search
 app.get("/get-status", (req, res) => {
   db.get("SELECT * FROM Main ORDER BY id DESC LIMIT 1", [], (err, row) => {
     if (err) {
@@ -70,6 +83,7 @@ app.get("/get-status", (req, res) => {
   });
 });
 
+//Return 8 rows of the database to display to the user as their history
 app.get("/get-history", (req, res) => {
   db.all("SELECT * FROM Main ORDER BY id DESC LIMIT 8", [], (err, rows) => {
     if (err) {
@@ -79,6 +93,7 @@ app.get("/get-history", (req, res) => {
   });
 });
 
+//Will return all instances of a certain site in the database so that the amount of times it has been search can be displayed to the user.
 app.get("/get-stats", (req, res) => {
   let website = req.query.website;
   db.all("SELECT id FROM Main WHERE website = '" + website + "'", [], (err, rows) => {
@@ -90,6 +105,7 @@ app.get("/get-stats", (req, res) => {
   })
 })
 
+//The port that the service runs on locally and the link to click on
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- website/script.js
+++ website/script.js
@@ -1,18 +1,24 @@
+//Code written by our group and Professor Othman
 document.addEventListener("DOMContentLoaded", () => {
+  //When one of these pages are loaded a different set of events happen
   const checkForm = document.getElementById("check-form");
   const statusText = document.getElementById("status-text");
   const historyTable = document.getElementById("history-table");
 
+  //if the check form page is loaded
   if (checkForm) {
     checkForm.addEventListener("submit", async (e) => {
+      //get the website the user entered and append https:// to it
       e.preventDefault();
       var website = document.getElementById("website").value;  
       website = "https://" + website;
 
+      //call the app.js method to check the status of the site
       try {
         const response = await fetch(`/check-status?website=${encodeURIComponent(website)}`);
         const data = await response.json();
 
+        //push the response to the webpage
         if (data.status) {
           localStorage.setItem("lastStatus", JSON.stringify(data));
           location.href = "Website_Search.html";
@@ -23,16 +29,20 @@ document.addEventListener("DOMContentLoaded", () => {
         console.error(err);
       }
     });
+    //when the page that displays the status of a site is loaded these events happen
   } else if (statusText) {
     (async () => {
+      //get the status that was stored in local memory form the check status
       const data = JSON.parse(localStorage.getItem("lastStatus"));
 
+      //push data to the site
       if (data) {
         statusText.textContent = `The website ${data.website} is ${data.status}.`;
       } else {
         statusText.textContent = "Error fetching website status.";
       }
 
+      //Tells the user the number of times that the site they search has been searched
       var i = 0;
       const responseStats = await fetch(`/get-stats?website=${encodeURIComponent(data.website)}`);
       const dataStats = await responseStats.json();
@@ -43,6 +53,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
       document.getElementById("stats").innerHTML = "This website has been searched "+ i + " times recently!"
     })();
+    //events when the history table is loaded
   } else if (historyTable) {
     (async () => {
       try {
